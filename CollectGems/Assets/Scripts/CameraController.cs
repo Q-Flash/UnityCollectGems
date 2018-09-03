e40bbc4fcfa3a0a35d82cc883a5967c5e2e9eb50
Q-Flash <mubarakdcricketer@hotmail.com>
2018-09-03 12:51:36 -0400

    Added simple terrain and character
    -Character can run

---
+++ CollectGems/Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+	public float lookSmooth = 0.09f;
+	public Vector3 offsetFromTarget = new Vector3(0, -6, -8);
+	public float xtilt = 10;
+
+	Vector3 destination = Vector3.zero;
+	PlayerMovementController charController;
+	float rotateVel = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		SetCameraTarget (target);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		MoveToTarget ();
+		LookAtTarget ();
+	}
+
+	void SetCameraTarget(Transform t){
+		target = t;
+
+		if (target != null) {
+			if (target.GetComponent<PlayerMovementController> ()) {
+				charController = target.GetComponent<PlayerMovementController> ();
+			}
+
+		} else {
+			Debug.LogError ("Your camera needs a target");
+		}
+	}
+
+	void MoveToTarget(){
+		destination = charController.TargetRotation * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget(){
+		float eulerYAngle = Mathf.SmoothDampAngle (transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+		transform.rotation = Quaternion.Euler (transform.eulerAngles.x, eulerYAngle, 0);
+	}
+}
---
+++ CollectGems/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+	static Animator anim;
+
+	public float inputDelay = 0.1f;
+	public float forwardVel = 12;
+	public float rotateVel = 100;
+	Quaternion targetRotation;
+	Rigidbody rbody;
+	float forwardInput, turnInput;
+
+	public Quaternion TargetRotation{
+		get { return targetRotation; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody> ())
+			rbody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError ("Object does not have a rigid body");
+		forwardInput = turnInput = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput ();
+		Turn ();
+	}
+
+	void FixedUpdate(){
+		Run ();
+	}
+
+
+	void GetInput(){
+		forwardInput = Input.GetAxis ("Vertical");
+		turnInput = Input.GetAxis ("Horizontal");
+	}
+
+	void Run(){
+		if (Mathf.Abs (forwardInput) > inputDelay) {
+			//then move
+			anim.SetBool ("isRunning",true);
+			rbody.velocity = transform.forward * forwardInput * forwardVel;
+
+		} else {
+			//Don't move
+			anim.SetBool ("isRunning",false);
+			rbody.velocity = Vector3.zero;
+
+		}
+			
+	}
+
+	void Turn(){
+		targetRotation *= Quaternion.AngleAxis (rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		transform.rotation = targetRotation;
+	}
+}
